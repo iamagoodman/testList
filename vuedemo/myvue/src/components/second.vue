61c0eafe85a194519023eca2a8700b8fff02a413
Frank <980765465@qq.com>
2020-07-20 22:39:40 +0800

    test

--- vuedemo/myvue/src/components/second.vue
+++ vuedemo/myvue/src/components/second.vue
@@ -1,6 +1,8 @@
 <template>
     <div style="width: 100%;height: 200px;background: red">
       <h1>i am second page</h1>
+      <div class="container">fdsafsdafsafsafsdfsafsafsa</div>
+      <div class="box">13245646464534152341352</div>
     </div>
 </template>
 
@@ -11,6 +13,29 @@
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+  @import "../libs/style";
+  @width:20rem;
+  @height:100px;
+  @bgColor:#23fe21;
+  .greenbg{
+    background: #006600;
+  }
+  #colors(){
+    red:red;
+    blue:blue;
+    yellow:yellow
+  }
+  .container{
+    width: @width;
+    height: @height;
+    .greenbg();
+    color: #colors[yellow];
+  }
+  .box{
+    width: @width;
+    height:@height;
+    color: #baseColor[titleColor];
+    background: #baseColor[titleBg];
+  }
 </style>
